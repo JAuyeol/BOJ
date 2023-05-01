70e5e1d80f800f68cc4c1f76184e79e43650f4db
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-01 18:17:03 +0900

    [Bronze V] Title: 꼬마 정민, Time: 136 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,10 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-        System.out.println(a+b+c);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
+		
+		long n1 = Long.parseLong(strtk.nextToken());
+		long n2 = Long.parseLong(strtk.nextToken());
+		long n3 = Long.parseLong(strtk.nextToken());
+		
+		System.out.println(n1 + n2 + n3);
+	}
+
 }
