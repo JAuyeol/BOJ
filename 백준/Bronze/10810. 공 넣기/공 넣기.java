e157b59a554317ba6ba27f5c639148748097869d
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-05 14:35:17 +0900

    [Bronze III] Title: 공 넣기, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,29 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
 		
-		int busket = sc.nextInt(); // 5개의 바구니
-		int count = sc.nextInt(); // 4번 실행 
+		int basket = Integer.parseInt(strtk.nextToken());
+		int count = Integer.parseInt(strtk.nextToken()); 
 		
-		int n1, n2, ball = 0; // n1번째 바구니부터 n2번째 바구니, ball
+		int n1, n2, ball = 0;
 		
-		int array1[] = new int[busket]; // 5개의 배열(바구니) 생성 
+		int array1[] = new int[basket];
 		
-		for(int i=0;i<count;i++) { // 4번 실행
-			n1 = sc.nextInt();  // n1번째 바구니 입력받기 1번째 > 0번째
-			n2 = sc.nextInt(); // n2번째까지의 바구니 입력받기 2번째 바구니까지 > 1번째
-			ball = sc.nextInt(); // 넣을 공 입력받기 
+		for(int i=0;i<count;i++) {
+			strtk = new StringTokenizer(br.readLine()," ");
+			n1 = Integer.parseInt(strtk.nextToken())-1;
+			n2 = Integer.parseInt(strtk.nextToken())-1;
+			ball = Integer.parseInt(strtk.nextToken());
 			
-			for(int j=n1-1;j<=n2-1;j++) { // n1 ~ n2 
+			for(int j=n1;j<=n2;j++) {
 				array1[j] = ball;
 			}
 		}
 		
-		for(int i=0;i<busket;i++) {
+		br.close();
+		
+		for(int i=0;i<array1.length;i++) {
 			System.out.print(array1[i]+" ");
 		}
+		
 	}
 }
-
