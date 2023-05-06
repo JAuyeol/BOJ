0f004e46fafd043dbf5e50de20776b8049695921
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-06 11:15:41 +0900

    [Bronze II] Title: 공 바꾸기, Time: 168 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,31 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
 		
-		int busket = sc.nextInt(); 
-		int count = sc.nextInt(); 
+		int basket = Integer.parseInt(strtk.nextToken());
+		int count = Integer.parseInt(strtk.nextToken()); 
 		
-		int n1, n2, temp = 0; 
+		int n1, n2, temp = 0;
 		
-		int array1[] = new int[busket]; 
+		int array1[] = new int[basket];
 		
-		for(int i=0;i<array1.length;i++) { 
-			array1[i] = i+1; 
+		for(int i=0;i<array1.length;i++) {
+			array1[i] = i+1;
 		}
 		
 		for(int i=0;i<count;i++) {
-			n1 = sc.nextInt()-1; 
-			n2 = sc.nextInt()-1;
+			strtk = new StringTokenizer(br.readLine()," ");
+			n1 = Integer.parseInt(strtk.nextToken())-1;
+			n2 = Integer.parseInt(strtk.nextToken())-1;
 			
 			temp = array1[n1];
 			array1[n1] = array1[n2];
 			array1[n2] = temp;
-			
 		}
 		
-		sc.close();
+		br.close();
 		
 		for(int i=0;i<array1.length;i++) {
 			System.out.print(array1[i]+" ");
@@ -33,4 +38,3 @@ public class Main {
 		
 	}
 }
-
