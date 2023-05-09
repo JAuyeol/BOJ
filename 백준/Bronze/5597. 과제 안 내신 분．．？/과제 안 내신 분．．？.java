72ae8646d95a8d5525c1df444d37c41a26a26043
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-09 16:38:51 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int array1[] = new int[30]; // 0 ~ 29, 30개 
+		int array1[] = new int[30];  
 		int stu = 0;
 		
 		for(int i=0;i<array1.length;i++) {
@@ -11,15 +14,15 @@ public class Main {
 		}
 		
 		for(int i=0;i<28;i++) {
-			stu = sc.nextInt()-1;
-			array1[stu] = stu+1;		
+			stu = Integer.parseInt(br.readLine())-1; 
+			array1[stu] = stu+1;	
 		}
 		
 		for(int i=0;i<array1.length;i++) {
 			if(array1[i]==0) System.out.println(i+1);
 		}
 		
-		sc.close();
+		br.close();
 		
 	}
 
