f6247e3fb6b7ee8ca82bbe2f10d32517a351582a
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-12 12:58:29 +0900

    [Bronze II] Title: 나머지, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int array1[] = new int[10];
+		int compare[] = new int[42];
+		int count = 0, num = 0;
+		
+		for(int i=0;i<array1.length;i++) {
+			array1[i] = sc.nextInt();
+			num = array1[i] % 42; // 0,1,2 ,,, 41 까지 저장 
+			
+			compare[num] = num + 1;	// 0 > 0, 1 > 1, 2 > 2
+		}
+		for(int i=0;i<compare.length;i++) {
+			if(compare[i]!=0) count++;	
+		}
+		
+		System.out.println(count);
+		
+		
+	}
+
+}
