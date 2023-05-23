55c27c23e9ff80891b17cdb3bb5ff8f7dcfcce31
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-05-23 17:25:33 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
 		
-		int n = sc.nextInt(); 
+		int n = Integer.parseInt(strtk.nextToken());
+		
 		double score[] = new double[n]; 
 		double avg, max = 0,sum = 0;
 		
-		for(int i=0;i<score.length;i++) {
-			score[i] = sc.nextInt(); 
+		strtk = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			score[i] = Double.parseDouble(strtk.nextToken());
 			
 			if(score[i]>max) 
 				max = score[i]; 
@@ -19,11 +26,11 @@ public class Main {
 			score[i] = (score[i]/max)*100;
 			sum += score[i];
 		}
+		br.close();
 		
 		avg = sum / n;
 		
-        System.out.println(avg);
-		
+		System.out.println(avg);
 		
 		
 	}
