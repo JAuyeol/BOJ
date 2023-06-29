7c7780c2e570463c97928933d24a6beffa4d03a1
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-06-29 21:37:42 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,30 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String num1 = sc.next();
-		String num2 = sc.next();
+		String[] str = br.readLine().split(" ");
 		
-        sc.close();
-        
-        char[] re_num1 = new char[3];
-		char[] re_num2 = new char[3];
-		int n = 0;
-		
-		for(int i=2;i>=0;i--) {
-			re_num1[i] = num1.charAt(n);
-			re_num2[i] = num2.charAt(n);
-			n++; 
-		}
-		String str_num1 = new String(re_num1);
-		String str_num2 = new String(re_num2);
-		int a = Integer.parseInt(str_num1);
-		int b = Integer.parseInt(str_num2);
+		int a = Integer.parseInt(new StringBuilder().append(str[0]).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder().append(str[1]).reverse().toString());
 		
 		System.out.println(a>b?a:b);
 		
 		
 	}
-}
+
+}	
