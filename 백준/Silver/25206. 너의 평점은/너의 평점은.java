419073cc80b44b015a131ca424e92abeb626fc69
jgygy <92623804+JAuyeol@users.noreply.github.com>
2023-08-11 17:59:19 +0900

    [Silver V] Title: 너의 평점은, Time: 212 ms, Memory: 17840 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -4,48 +4,48 @@ public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 
-		double gpa = 0, credit_sum = 0;
-		char grade;
+		double result = 0, credit_sum = 0;
+		char c;
 				
 		for(int i=0;i<20;i++) {
 			double credit = 0; 
-			double sum = 0;
+			double grade = 0;
 			
 			String str_arr[] = sc.nextLine().split(" ");
-			grade = str_arr[2].charAt(0);
+			c = str_arr[2].charAt(0);
 			
 			credit += Double.parseDouble(str_arr[1]);
 			
-			switch(grade) {
+			switch(c) {
 			case 'A':
-				if(str_arr[2].charAt(1) == '+') sum += 4.5;
-				else sum += 4.0;
+				if(str_arr[2].charAt(1) == '+') grade += 4.5;
+				else grade += 4.0;
 				break;
 			case 'B':
-				if(str_arr[2].charAt(1) == '+') sum += 3.5;
-				else sum += 3.0;
+				if(str_arr[2].charAt(1) == '+') grade += 3.5;
+				else grade += 3.0;
 				break;
 			case 'C':
-				if(str_arr[2].charAt(1) == '+') sum += 2.5;
-				else sum += 2.0;
+				if(str_arr[2].charAt(1) == '+') grade += 2.5;
+				else grade += 2.0;
 				break;
 			case 'D':
-				if(str_arr[2].charAt(1) == '+') sum += 1.5;
-				else sum += 1.0;
+				if(str_arr[2].charAt(1) == '+') grade += 1.5;
+				else grade += 1.0;
 				break;
 			case 'F':
-				sum += 0;
+				grade += 0;
 				break;
 			case 'P':
-				sum += 0;
+				grade += 0;
 				credit = 0;
 				break;
 			}
 			credit_sum += credit;
-			gpa += credit*sum;
+			result += credit*grade;
 		}
 		sc.close();
 
-		System.out.println(gpa/credit_sum);
+		System.out.println(result/credit_sum);
 	}
 }
